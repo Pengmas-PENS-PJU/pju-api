a1acd27e9ebdaf1889d9b95e3441ff93c527a5c5
mirza27 <ramadhanimirza10@gmail.com>
2024-08-14 17:40:33 +0700

    add auto seeder

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ const dotenv = require("dotenv");
 const http = require("http");
 const router = require("./routes/index.js");
 const { initSocket } = require("./socket");
+const seeder = require("./prisma/seed.js");
 
 // config
 dotenv.config();
 const port = process.env.PORT || 5000;
 const app = express();
+
+//  run seeder
+await seeder();
 
 // alamat diizinkan cors
 app.use(
@@ -30,32 +34,6 @@ app.use(cookieParser());
 
 // path router
 app.use(router);
-
-// app.use("/", indexRouter);
-// app.use("/users", usersRouter);
-// app.use("/sensor", sensorRouter);
-
-// // view engine setup
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "jade");
-
-// app.use(express.static(path.join(__dirname, "public")));
-
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render("error");
-// });
 
 // buat server HTTP
 const server = http.createServer(app);
