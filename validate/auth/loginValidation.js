d52bf3b73d8980a24b0976d00425bcd465588475
Bayu Hadi Leksana <bayuhadi863@gmail.com>
2024-10-12 00:13:12 +0700

    feat(auth): update the auth login by generate access token and refresh token

---
+++ validate/auth/loginValidation.js
@@ -0,0 +1,19 @@
+const { body, validationResult } = require('express-validator');
+
+const loginValidation = [
+  body('username_email').isString().withMessage('Username or email must be a string').notEmpty().withMessage('Username or email is required'),
+
+  body('password').isString().withMessage('Password must be a string').notEmpty().withMessage('Password is required'),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
+
+module.exports = {
+  loginValidation,
+};
